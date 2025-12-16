afab4d65b06e63347edca2fd343e8036b181921a
LorranAlexandreTavares <lorranalexandre27@gmail.com>
2025-12-16 11:05:58 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>Controle Financeiro Pessoal</h1>
+      <p>Seu sistema está online.</p>
+    </main>
+  );
+}
